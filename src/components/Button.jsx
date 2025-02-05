0322e731ce78a42f380e6eb4c5c3a31c1ad21dab
raja <rajababuchauhan500@gmail.com>
2025-02-05 14:44:48 +0100

    button test

---
+++ src/components/Button.jsx
@@ -0,0 +1,7 @@
+export const Button = () => {
+    return (
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold p-4 rounded">
+            Button
+        </button>
+    );
+};
